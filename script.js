1623e17defb771409181f815999d2087197e7e84
Christine Nguyen <ctine.ngyn@gmail.com>
2021-03-12 17:05:38 -0800

    Baby steps for js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Variables 
+var city = "";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty = $("#humidity");
+var currentWSpeed = $("#wind-speed");
+var currentUvindex = $("#uv-index");
+
 // Store API key
-const apiKey = "c0b5527e46e133285bbf1cfe6786f6bf";
+// After user searches up city, it will display current weather
+var apiKey = "c0b5527e46e133285bbf1cfe6786f6bf";
+
+function displayWeather(event){
+    event.preventDefault();
+
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
 
