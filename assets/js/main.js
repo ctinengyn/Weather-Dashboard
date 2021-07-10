2116da5890efd3a573db95089409340190146bd8
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-10 02:51:53 -0700

    Response from API

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,41 @@ $("#searchTerm").keypress(function(event) {
 		$("#searchBtn").click(); 
 	} 
 });
+
+$("#searchBtn").on("click", function() {
+
+	$('#forecastH5').addClass('show');
+  
+	// get the value of the input from user
+	city = $("#searchTerm").val();
+	
+	// clear input box
+	$("#searchTerm").val("");  
+  
+	// full url to call api
+	const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+  
+	$.ajax({
+	  url: queryUrl,
+	  method: "GET"
+	})
+	.then(function (response){
+  
+	  console.log(response)
+  
+	  console.log(response.name)
+	  console.log(response.weather[0].icon)
+  
+	  let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+	  console.log(Math.floor(tempF))
+  
+	  console.log(response.main.humidity)
+  
+	  console.log(response.wind.speed)
+  
+	  getCurrentConditions(response);
+	  getCurrentForecast(response);
+	  makeList();
+  
+	  })
+	});
